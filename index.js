1e52f3c95dcd469b9477c51a74c142feebe3e1f7
gbox3d <gbox3d@gmail.com>
2022-10-23 10:52:18 +0000

    mainpage login check

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const host_url = ''
 
 async function main() {
+
+    const mainContainer = document.querySelector('#mainContainer');
+    mainContainer.style.display = 'none';
     
 
     try {
+
+        //고르인 정보 얻기 
         let res = await (await (fetch(`${host_url}/api/v2/users/`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/text',
-                // 'auth-token': localStorage.getItem('authToken')
                 'authorization': localStorage.getItem('jwt_token')
             }
         }))).json();
-
         console.log(res);
+
         if (res.r === 'ok') {
             document.querySelector('#authtoken').hidden = true;
             document.querySelector('#authStatus').hidden = false;
-
             document.querySelector('#authStatus').querySelector('h3').innerHTML = `login ok ${res.user.userName}`;
+            mainContainer.style.display = 'block';
         }
-
-
+        else {
+            document.querySelector('#authtoken').hidden = false;
+            document.querySelector('#authStatus').hidden = true;
+            document.querySelector('#authStatus').querySelector('h3').innerHTML = `login failed`;
+        }
     }
     catch (err) {
         console.log(err)
